40a62706c36e1628906f8f75309d076e15baba5e
Doigonist <doigonist@gmail.com>
2025-06-20 02:07:20 +0200

    0.4

--- uploader.php
+++ uploader.php
@@ -3,5 +3,5 @@ $version = (float)file_get_contents('version.txt');
 exec('git add .',$out);
 exec("git commit -m $version",$out1);
 exec("git push origin master",$out2);
-// print_r($out2);
 file_put_contents('version.txt',$version + 0.1);
+header('Location: admin-page.php');
