915f3b15735798a6118d1592ee3e079ae2313b4f
Doigonist <doigonist@gmail.com>
2025-06-20 02:06:44 +0200

    0.3

--- uploader.php
+++ uploader.php
@@ -1,7 +1,7 @@
 <?php
 $version = (float)file_get_contents('version.txt');
 exec('git add .',$out);
-exec("git commit -m '$version'",$out1);
+exec("git commit -m $version",$out1);
 exec("git push origin master",$out2);
 // print_r($out2);
 file_put_contents('version.txt',$version + 0.1);
